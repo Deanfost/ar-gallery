31be863dff8b6da57a382258555336fa990f2af5
Dean Foster <Deanfoster45@gmail.com>
2018-07-14 23:58:57 -0400

    Work on ARActivity

--- app/src/main/java/com/example/deanf/arvideo/ARActivity.java
+++ app/src/main/java/com/example/deanf/arvideo/ARActivity.java
@@ -18,24 +18,35 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class ARActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.1;
     private static final String LOG_TAG = "MediaSession";
 
+    private final MovieClipRenderer movieClipRenderer = new MovieClipRenderer();
 
     private ArFragment arFragment;
     private ViewRenderable screenRenderable;
-    private ArSceneView arSceneView;
+    Session session;
 
     MediaSessionCompat mediaSession;
     PlaybackStateCompat.Builder stateBuilder;
@@ -47,6 +58,8 @@ public class ARActivity extends AppCompatActivity {
     private ImageButton btnFoward30;
 
     private String filepath;
+    boolean isDoneLoading = false;
+    boolean hasPlacedScreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +69,17 @@ public class ARActivity extends AppCompatActivity {
             return;
         }
 
+        try {
+            session = new Session(this);
+        } catch (UnavailableArcoreNotInstalledException | UnavailableApkTooOldException | UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        }
+
+        // Get references
         setContentView(R.layout.activity_ar);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        filepath = getIntent().getStringExtra("Filepath");
+        layout = findViewById(R.id.ar_layout);
 
         // Setup Media Player components
         mediaSession = new MediaSessionCompat(this, LOG_TAG);
@@ -78,45 +100,87 @@ public class ARActivity extends AppCompatActivity {
             }
         });
 
-        
 
-
-        // Create the renderable
-        ViewRenderable.builder()
+        // Create the renderable in the background
+        CompletableFuture<ViewRenderable> renderable = ViewRenderable.builder()
                 .setView(this, R.layout.widget_video)
-                .build()
-                .thenAccept(renderable -> this.screenRenderable = renderable);
+                .build();
 
-        // Get references
-        filepath = getIntent().getStringExtra("Filepath");
-        layout = findViewById(R.id.ar_layout);
-        videoView =  screenRenderable.getView().findViewById(R.id.video_view);
-        btnBack10 = screenRenderable.getView().findViewById(R.id.btn_back);
-        btnToggle = screenRenderable.getView().findViewById(R.id.btn_toggle);
-        btnFoward30 = screenRenderable.getView().findViewById(R.id.btn_forward);
+        CompletableFuture.allOf(renderable).handle((notUsed, throwable) -> {
+            if(throwable != null) {
+                Snackbar snackbar =
+                        Snackbar.make(layout, "Unable to load renderable.", 5000);
+                snackbar.show();
+                return null;
+            }
+
+//            try {
+//                this.screenRenderable = renderable.get();
+//
+//                // Get view references
+//                videoView = screenRenderable.getView().findViewById(R.id.video_view);
+//                btnBack10 = screenRenderable.getView().findViewById(R.id.btn_back);
+//                btnToggle = screenRenderable.getView().findViewById(R.id.btn_toggle);
+//                btnFoward30 = screenRenderable.getView().findViewById(R.id.btn_forward);
+//
+//                // Done loading
+//                isDoneLoading = true;
+//
+//            } catch (InterruptedException | ExecutionException e) {
+//                e.printStackTrace();
+//            }
+
+            return null;
+        });
 
         // Setup a tap listener
         arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
             @Override
             public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
-                // Create an anchor
-                Anchor anchor = hitResult.createAnchor();
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
+                if(isDoneLoading && !hasPlacedScreen) {
+                    // Create an anchor
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-                // Create a transformable screen and add to the anchor
-                TransformableNode screen = new TransformableNode(arFragment.getTransformationSystem());
-                screen.setParent(anchorNode);
-                screen.setRenderable(screenRenderable);
-                screen.select();
+                    // Modify rotation
+//                    anchorNode.setWorldRotation(hitResult.getHitPose().);
 
-                // Play the video on the renderable
+//                    // Create a transformable screen and add to the anchor
+//                    TransformableNode screen = new TransformableNode(arFragment.getTransformationSystem());
+//                    screen.setParent(anchorNode);
+//                    screen.setRenderable(screenRenderable);
+//                    screen.select();
 
+                    // Play the video on the renderable
+                    if(!movieClipRenderer.isStarted()) {
+                        float[] poseMatrix = new float[16];
+                        float[] viewMatrix = new float[16];
+                        float[] projectionMatrix = new float[16];
+                        try {
+                            session.update().getCamera().getViewMatrix(viewMatrix, 0);
+                            session.update().getCamera().getProjectionMatrix(projectionMatrix, 0, 0, 1);
+                        } catch (CameraNotAvailableException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            movieClipRenderer.play(filepath, ARActivity.this);
+                            anchor.getPose().toMatrix(poseMatrix, 0);
+                            movieClipRenderer.update(poseMatrix, .25f);
+                            movieClipRenderer.draw(anchor.getPose(), viewMatrix, projectionMatrix);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    hasPlacedScreen = true;
+                }
             }
         });
 
         // Display a snackbar to tell the user how to get started
-        Snackbar snackbar = Snackbar.make(layout, "Tap to place a display on a discovered surface.", 5000);
+        Snackbar snackbar =
+                Snackbar.make(layout, "Tap to place a display on a discovered surface.", 5000);
         snackbar.show();
     }
 
