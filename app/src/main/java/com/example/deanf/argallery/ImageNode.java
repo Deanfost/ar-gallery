1399894280b970edcd65a3ba4c8f58d057f036b5
Dean Foster <Deanfoster45@gmail.com>
2018-07-19 21:41:30 -0400

    Re-add meta-data library with Maven, parse meta-data from image

--- app/src/main/java/com/example/deanf/argallery/ImageNode.java
+++ app/src/main/java/com/example/deanf/argallery/ImageNode.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.MotionEvent;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.ar.sceneform.HitTestResult;
@@ -12,10 +13,12 @@ import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 
+import java.io.File;
+
 public class ImageNode extends Node implements Node.OnTapListener {
     private Bitmap image;
     private float imgWidth, imgHeight;
-    private double imageScale;
+//    private double imageScale;
     public boolean imageLoaded, metaLoaded = false;
     private String filepath;
 
@@ -33,7 +36,7 @@ public class ImageNode extends Node implements Node.OnTapListener {
 
         this.filepath = filePath;
         this.context = context;
-        this.imageScale = imageScale;
+//        this.imageScale = imageScale;
         setOnTapListener(this);
         this.setEnabled(false);
     }
@@ -75,8 +78,22 @@ public class ImageNode extends Node implements Node.OnTapListener {
                     metaDataNode.setRenderable(viewRenderable);
                     metaViewRenderable = viewRenderable;
 
-                    // Set metadata info
+                    // Get metadata view references
+                    TextView metaName = metaViewRenderable.getView().findViewById(R.id.meta_name);
+                    TextView metaTime = metaViewRenderable.getView().findViewById(R.id.meta_time);
+                    TextView metaRes = metaViewRenderable.getView().findViewById(R.id.meta_res);
+                    TextView metaSize = metaViewRenderable.getView().findViewById(R.id.meta_size);
+                    TextView metaLocation = metaViewRenderable.getView().findViewById(R.id.meta_location);
 
+                    // Get metadata of images
+                    MetaParser metaParser = new MetaParser(new File(filepath));
+
+                    // Assign data
+                    metaName.setText(metaParser.getFileName());
+                    metaTime.setText(metaParser.getFileTime());
+                    metaRes.setText(metaParser.getFileRes());
+                    metaSize.setText(metaParser.getFileSize());
+                    metaLocation.setText(metaParser.getTakenLocation());
 
                     // Done loading
                     metaLoaded = true;
---
+++ app/src/main/java/com/example/deanf/argallery/MetaParser.java
@@ -0,0 +1,97 @@
+package com.example.deanf.argallery;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDescriptor;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDescriptor;
+import com.drew.metadata.exif.GpsDirectory;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MetaParser {
+    private final Double B_TO_MB_RATIO = 0.000001;
+
+    private Metadata metadata;
+    private ExifSubIFDDirectory exifSubIFDDirectory;
+    private ExifSubIFDDescriptor exifSubIFDDescriptor;
+    private GpsDirectory gpsDirectory;
+    private GpsDescriptor gpsDescriptor;
+    private File image;
+
+    public MetaParser(File image) {
+        try {
+            this.metadata = ImageMetadataReader.readMetadata(image);
+            exifSubIFDDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            exifSubIFDDescriptor = new ExifSubIFDDescriptor(exifSubIFDDirectory);
+            gpsDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+            gpsDescriptor = new GpsDescriptor(gpsDirectory);
+            this.image = image;
+        } catch (ImageProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getFileName() {
+        // Get filename
+        return image.getName();
+    }
+
+    public String getFileTime() {
+        // Get the time the image was taken
+        Date date = exifSubIFDDirectory.getDateOriginal(TimeZone.getDefault());
+        if(date != null) {
+            DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm:aa");
+            return df.format(date);
+        }
+        else {
+            return "Date Taken - N/A";
+        }
+    }
+
+    public String getFileRes() {
+        // Get width x height of image
+        String width = exifSubIFDDescriptor.getExifImageWidthDescription();
+        String height = exifSubIFDDescriptor.getExifImageHeightDescription();
+        if(width != null && height != null) {
+            width = width.substring(0, width.lastIndexOf(" "));
+            height = height.substring(0, height.lastIndexOf(" "));
+            return width + "x" + height;
+        }
+        else {
+            return "Resolution - N/A";
+        }
+    }
+
+    public String getFileSize() {
+        // Get size of image in bytes then convert to mb
+        Double fileSize = image.length() * B_TO_MB_RATIO;
+        return (fileSize != 0) ? Double.toString(Math.round(fileSize * 100.0) / 100.0) + "MB" : "Size - N/A";
+    }
+
+    public String getTakenLocation() {
+        // Get coordinates of image's location and convert from DMS to decimal
+        String lat = gpsDescriptor.getGpsLatitudeDescription();
+        String lon = gpsDescriptor.getGpsLongitudeDescription();
+        return (lat != null && lon != null) ? lat + ", " + lon : "Location - N/A";
+    }
+
+    public void logAllData() {
+        // Log all applicable metadata
+        for(Directory dir : metadata.getDirectories()) {
+            for(Tag tag : dir.getTags()) {
+                System.out.println(dir + " " + tag);
+            }
+        }
+    }
+}
